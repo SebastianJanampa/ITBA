7c54a0d65456e1d5926e428c3b44c938ea85e7c0
Agustina Osimani <aosimani@itba.edu.ar>
2021-04-01 19:17:43 -0300

    ADD - get data for ej3

---
+++ TP1/BayesianNetwork.py
@@ -0,0 +1,4 @@
+class BayesianNetwork:
+    def __init__(self, dataset, dependency_graph):
+        self.dataset = dataset
+        self.dependency_graph = dependency_graph
--- TP1/main.py
+++ TP1/main.py
@@ -2,6 +2,8 @@ import argparse
 import pandas as pd
 import openpyxl
 
+from TP1.BayesianNetwork import BayesianNetwork
+
 
 def british_preferences(dataset_path: str, scones: bool, cerveza: bool, whisky: bool, avena: bool, futbol: bool):
     dataset = pd.read_excel(dataset_path)
@@ -15,7 +17,12 @@ def argentine_news(dataset_path: str):
 
 def admissions(dataset_path: str):
     dataset = pd.read_csv(dataset_path)
-    pass
+    dataset['gre'] = dataset['gre'].apply(lambda value: 1 if value >= 500 else 0)
+    dataset['gpa'] = dataset['gpa'].apply(lambda value: 1 if value >= 3 else 0)
+
+    dependency_graph = {'admit': ['gre', 'rank', 'gpa'], 'gre': ['rank'], 'gpa': ['rank'], 'rank': []}
+
+    BayesianNetwork(dataset, dependency_graph)
 
 
 def str2bool(v):
@@ -23,7 +30,8 @@ def str2bool(v):
 
 
 def main():
-    argparser = argparse.ArgumentParser(description='Aprendizaje Automático: Método de Bayes', formatter_class=argparse.RawTextHelpFormatter)
+    argparser = argparse.ArgumentParser(description='Aprendizaje Automático: Método de Bayes',
+                                        formatter_class=argparse.RawTextHelpFormatter)
     argparser.register('type', 'bool', str2bool)
     argparser.add_argument('-e', '--exercise', type=int, choices=[1, 2, 3], required=True)
     argparser.add_argument('-d', '--dataset_path', required=True)
