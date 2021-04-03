296bb693e6c3ea93cda77f3fa0f34d824864cc57
Agustina Osimani <aosimani@itba.edu.ar>
2021-04-03 01:38:55 -0300

    ADD - BayesianNetwork

--- TP1/main.py
+++ TP1/main.py
@@ -44,14 +44,21 @@ def argentine_news(dataset_path: str):
     # drawRocCurve()
 
 
-def admissions(dataset_path: str):
+def admissions(dataset_path: str, probability_request: str):
     dataset = pd.read_csv(dataset_path)
     dataset['gre'] = dataset['gre'].apply(lambda value: 1 if value >= 500 else 0)
     dataset['gpa'] = dataset['gpa'].apply(lambda value: 1 if value >= 3 else 0)
 
     dependency_graph = {'admit': ['gre', 'rank', 'gpa'], 'gre': ['rank'], 'gpa': ['rank'], 'rank': []}
 
-    BayesianNetwork(dataset, dependency_graph)
+    bn = BayesianNetwork(dataset, dependency_graph)
+    request_elements = probability_request.split('|')
+    if len(request_elements) == 1:
+        probability = bn.get_probability(request_elements[0])
+    else:
+        probability = bn.get_probability(request_elements[0] + ',' + request_elements[1]) \
+                      / bn.get_probability(request_elements[1])
+    print(f'P({probability_request}) = {probability}')
 
 
 def get_confusion_matrix(classes):
@@ -122,13 +129,14 @@ def main():
     argparser.add_argument('-w', '--whisky', type='bool', choices=[True, False], required=False)
     argparser.add_argument('-a', '--avena', type='bool', choices=[True, False], required=False)
     argparser.add_argument('-f', '--futbol', type='bool', choices=[True, False], required=False)
+    argparser.add_argument('-r', '--probability_request', required=False)
     args = vars(argparser.parse_args())
 
     switcher = {
         1: lambda: british_preferences(args['dataset_path'], args['scones'], args['cerveza'], args['whisky'],
                                        args['avena'], args['futbol']),
         2: lambda: argentine_news(args['dataset_path']),
-        3: lambda: admissions(args['dataset_path']),
+        3: lambda: admissions(args['dataset_path'], args['probability_request']),
     }
     switcher.get(args['exercise'])()
 
